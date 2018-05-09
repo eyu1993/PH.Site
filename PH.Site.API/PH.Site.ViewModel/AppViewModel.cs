2b5611b196ed0e88165b3461f650b629cc7439f4
pinhua.wu <pinhua.wu@pery.com>
2018-05-09 17:07:46 +0800

    添加图片

--- PH.Site.API/PH.Site.ViewModel/AppViewModel.cs
+++ PH.Site.API/PH.Site.ViewModel/AppViewModel.cs
@@ -7,11 +7,15 @@ namespace PH.Site.ViewModel
 {
     public class AppViewModel : BaseViewModel
     {
+        public AppViewModel()
+        {
+            Categories = new List<AppCategory>();
+        }
         public Guid AppId { get; set; }
         public string AppName { get; set; }
         public string Image { get; set; }
         public string CodeUrl { get; set; }
         public string Remark { get; set; }
-        public IList<AppCategory> Categories { get; set; }
+        public virtual ICollection<AppCategory> Categories { get; set; }
     }
 }
